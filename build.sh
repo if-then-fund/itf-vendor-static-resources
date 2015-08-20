fe59e2ce4b69a476961376380798d1dbde08945b
Joshua Tauberer <jt@occams.info>
2015-08-19 20:31:51 -0400

    adding bootstrap, jquery, moment

--- build.sh
+++ build.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
+mkdir -p static/css static/js static/fonts
+
+# bootstrap
+BOOTSTRAP=3.3.0
+wget -O static/css/bootstrap.min.css https://maxcdn.bootstrapcdn.com/bootstrap/$BOOTSTRAP/css/bootstrap.min.css
+wget -O static/css/bootstrap-theme.min.css https://maxcdn.bootstrapcdn.com/bootstrap/$BOOTSTRAP/css/bootstrap-theme.min.css
+wget -O static/js/bootstrap.min.js https://maxcdn.bootstrapcdn.com/bootstrap/$BOOTSTRAP/js/bootstrap.min.js
+
+# jquery, etc.
+wget -4 -O static/js/jquery.min.js https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js
+wget -4 -O static/js/moment.min.js https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.10.3/moment.min.js
+
+# webfonts
 webfont-dl \
 	"http://fonts.googleapis.com/css?family=`cat fonts.txt | python3 fonts.py`" \
 	-o static/fonts/fonts.css
+
